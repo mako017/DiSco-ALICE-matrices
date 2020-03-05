793c15defb5f6511346ab16d6e0294db53b4d539
mako017 <54231461+mako017@users.noreply.github.com>
2020-03-05 13:08:50 +0100

    fallback debug2

--- DataHandling.js
+++ DataHandling.js
@@ -166,12 +166,12 @@ function openLock() {
     if (Participant.user.trim() === "download") {
         document.getElementById("lockscreen").classList.add("hidden");
         document.getElementById("download-div").classList.remove("hidden");
-        // document.getElementById("download-txt").value = jtc2(getLocal());
+        document.getElementById("download-txt").value = jtc2(getLocal());
 
-        // document.getElementById("download-txt").select();
-        // document.getElementById("download-txt").setSelectionRange(0, 9999999); /*For mobile devices*/
+        document.getElementById("download-txt").select();
+        document.getElementById("download-txt").setSelectionRange(0, 9999999); /*For mobile devices*/
       
-        // document.execCommand("copy");
+        document.execCommand("copy");
       
         alert("Copied the text");
     }
