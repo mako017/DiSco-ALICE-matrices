ee139abb133549b7343ed7e9e0c5f4f9d432b1b5
Marco Koch <marco.koch@uni-saarland.de>
2021-05-17 13:41:19 +0200

    prettier formatting

--- js/navigation.js
+++ js/navigation.js
@@ -1,49 +1,51 @@
-document.getElementById("send").addEventListener("click",getResponse,false);
-document.getElementById("inst-btn").addEventListener("click",switchInst,false);
-document.getElementById("back-btn").addEventListener("click",resetInst,false);
-document.getElementById("lock-btn").addEventListener("click",openLock,false);
+document.getElementById("send").addEventListener("click", getResponse, false);
+document
+	.getElementById("inst-btn")
+	.addEventListener("click", switchInst, false);
+document.getElementById("back-btn").addEventListener("click", resetInst, false);
+document.getElementById("lock-btn").addEventListener("click", openLock, false);
 
 function resetInst() {
-    document.getElementById("inst-page3").classList.toggle("hidden");
-    Settings.instPage = 1;
-    document.getElementById("inst-page1").classList.toggle("hidden");
-    document.getElementById("inst-btn").innerHTML = "Weiter";
+	document.getElementById("inst-page3").classList.toggle("hidden");
+	Settings.instPage = 1;
+	document.getElementById("inst-page1").classList.toggle("hidden");
+	document.getElementById("inst-btn").innerHTML = "Weiter";
 }
 
 function switchInst() {
-switch (Settings.instPage) {
-    case 0:
-        document.getElementById("inst-page1").classList.toggle("hidden");
-        document.getElementById("inst-page2").classList.toggle("hidden");
-        break;
-    case 1:
-        document.getElementById("inst-page2").classList.toggle("hidden");
-        document.getElementById("inst-page3").classList.toggle("hidden");
-        document.getElementById("back-btn").classList.toggle("hidden");
-        document.getElementById("inst-btn").innerHTML = "Start";
-        break;
-    case 2:
-        document.getElementById("stavmat-instructions").classList.toggle("hidden");
-        document.getElementById("stavmat-test").classList.toggle("hidden");
-        resetTime();
-        Settings.logTime = Date.now();
-        timelimit(1080);
-    default:
-        break;
-    }
-    Settings.instPage++;
+	switch (Settings.instPage) {
+		case 0:
+			document.getElementById("inst-page1").classList.toggle("hidden");
+			document.getElementById("inst-page2").classList.toggle("hidden");
+			break;
+		case 1:
+			document.getElementById("inst-page2").classList.toggle("hidden");
+			document.getElementById("inst-page3").classList.toggle("hidden");
+			document.getElementById("back-btn").classList.toggle("hidden");
+			document.getElementById("inst-btn").innerHTML = "Start";
+			break;
+		case 2:
+			document
+				.getElementById("stavmat-instructions")
+				.classList.toggle("hidden");
+			document.getElementById("stavmat-test").classList.toggle("hidden");
+			resetTime();
+			Settings.logTime = Date.now();
+			timelimit(1080);
+		default:
+			break;
+	}
+	Settings.instPage++;
 }
 
-document.getElementById("DELETE").addEventListener("click",function(){
-    if (confirm("Sollen die Daten wirklich unwiderruflich gelöscht werden?")) {
-        localStorage.clear();
-    }
-},false);
-
-document.addEventListener('gesturestart', function (e) {
-    e.preventDefault();
+document.addEventListener("gesturestart", function (e) {
+	e.preventDefault();
 });
 
-window.addEventListener('load', function() {
-    new FastClick(document.body);
-}, false);
+window.addEventListener(
+	"load",
+	function () {
+		new FastClick(document.body);
+	},
+	false
+);
