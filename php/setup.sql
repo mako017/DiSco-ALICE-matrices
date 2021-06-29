8b42e85026556830093f1b6446dfce8389930b99
marco Koch <marco.koch@uni-saarland.de>
2021-06-29 11:27:30 +0200

    updated SQL setup

--- php/setup.sql
+++ php/setup.sql
@@ -1,2 +1,16 @@
-CREATE DATABASE `matrices`CHARSET=utf8 COLLATE utf8_unicode_ci;
-CREATE TABLE `matrices`.`matrices_results` ( `counter` INT NOT NULL AUTO_INCREMENT , `VPCode` TEXT NOT NULL , `persCode` TEXT NOT NULL , `nickName` TEXT NOT NULL , `RT` LONGTEXT NOT NULL , `response` LONGTEXT NOT NULL , `log` LONGTEXT NOT NULL , PRIMARY KEY (`counter`)) ENGINE = InnoDB CHARSET=utf8 COLLATE utf8_unicode_ci;
+CREATE DATABASE IF NOT EXISTS `matrices` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+DROP TABLE IF EXISTS `matrices_results`;
+CREATE TABLE `matrices_results` (
+  `counter` int(11) NOT NULL,
+  `VPCode` text COLLATE utf8_unicode_ci NOT NULL,
+  `persCode` text COLLATE utf8_unicode_ci NOT NULL,
+  `nickName` text COLLATE utf8_unicode_ci NOT NULL,
+  `RT` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `response` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `log` longtext COLLATE utf8_unicode_ci NOT NULL,
+  `phpCode` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+ALTER TABLE `matrices_results`
+  ADD PRIMARY KEY (`counter`);
+ALTER TABLE `matrices_results`
+  MODIFY `counter` int(11) NOT NULL AUTO_INCREMENT;
